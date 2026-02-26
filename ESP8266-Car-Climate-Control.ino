dc1d345872a7222713d8545ffc7f33468ad26853
nerisuyu <nerisuyu@gmail.com>
2026-02-26 16:09:16 +0300

    Setup initialization of Wifi Web Server

--- ESP8266-Car-Climate-Control.ino
+++ ESP8266-Car-Climate-Control.ino
@@ -18,11 +18,51 @@
 const char *ssid = APSSID;
 const char *password = APPSK;
 
+IPAddress my_ip = 0;
+ESP8266WebServer server(80);
+
+
+///////////////////////////////////////////////////////////////////////////////
+////Wifi Server
+
+void setupWifiServer(){
+// setup WiFi Network
+  Serial.println();
+  Serial.println();
+  Serial.println("Configuring access point...");
+  Serial.println();
+  WiFi.softAP(ssid, password);
+  Serial.print("SSID: ");
+  Serial.println(ssid);
+  Serial.print("PASSWORD: ");
+  Serial.println(password);
+
+  
+
+  my_ip = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(my_ip);
+
+  server.on("/", handleRoot);
+  server.begin();
+
+  Serial.println();
+  Serial.println("HTTP server started");
+}
+
+void handleRoot() {
+  server.send(200, "text/html", root_webpage);
+}
+
+///////////////////////////////////////////////////////////////////////////////
 
 void setup() {
+  Serial.begin(9600);
+  delay(500);
 
+  setupWifiServer();
 }
 
 void loop() {
-
+  server.handleClient();
 }
